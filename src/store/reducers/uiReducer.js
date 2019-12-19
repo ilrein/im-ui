7a720cfca343bd390ee79737bfa590336f0bdf92
Ilia <ilia@acies.ai>
2019-12-19 18:00:15 -0500

    stashes proprties for uiReducer to localstorage, in order to persist visible table rows

--- src/store/reducers/uiReducer.js
+++ src/store/reducers/uiReducer.js
@@ -1,53 +1,63 @@
+const defaultProperties = [
+  {
+    key: 'id',
+    visible: true,
+  },
+  {
+    key: 'title',
+    visible: true,
+  },
+  {
+    key: 'body_html',
+    visible: true,
+  },
+  {
+    key: 'vendor',
+    visible: true,
+  },
+  {
+    key: 'product_type',
+    visible: true,
+  },
+  {
+    key: 'created_at',
+    visible: true,
+  },
+  {
+    key: 'handle',
+    visible: true,
+  },
+  {
+    key: 'updated_at',
+    visible: true,
+  },
+  {
+    key: 'published_at',
+    visible: false,
+  },
+  {
+    key: 'tags',
+    visible: true,
+  }
+];
+
 function uiReducer(state = {
-  properties: [
-    {
-      key: 'id',
-      visible: true,
-    },
-    {
-      key: 'title',
-      visible: true,
-    },
-    {
-      key: 'body_html',
-      visible: true,
-    },
-    {
-      key: 'vendor',
-      visible: true,
-    },
-    {
-      key: 'product_type',
-      visible: true,
-    },
-    {
-      key: 'created_at',
-      visible: true,
-    },
-    {
-      key: 'handle',
-      visible: true,
-    },
-    {
-      key: 'updated_at',
-      visible: true,
-    },
-    {
-      key: 'published_at',
-      visible: false,
-    },
-    {
-      key: 'tags',
-      visible: true,
-    }
-  ],
+  properties: localStorage.getItem('im-visible-props-config') === null ? defaultProperties : JSON.parse(localStorage.getItem('im-visible-props-config')).properties,
 }, action) {
   switch (action.type) {
     case 'CHANGE_PROP_VISIBILITY':
       const itemToChange = state.properties.find(prop => prop.key === action.key);
       itemToChange.visible = action.payload;
 
-      return state;
+      const newState = {
+        properties: [
+          ...state.properties,
+        ]
+      };
+
+      localStorage.setItem('im-visible-props-config', JSON.stringify(newState));
+
+      return newState;
     default:
       return state;
   }
