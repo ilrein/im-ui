3ca6907842d4de49549f39278f0d57100c56766a
Ilia <ilia@acies.ai>
2019-12-20 17:05:16 -0500

    metafields table setup

--- src/components/MetafieldsTable/MetafieldsTable.js
+++ src/components/MetafieldsTable/MetafieldsTable.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import {
+  Table,
+} from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const MetafieldsTable = () => {
+const MetafieldsTable = ({
+  es,
+}) => {
   return (
-    <div>MetafieldsTable</div>
+    <Table celled striped>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell colSpan='3'>
+            Metafields
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {
+          es.products.list.map(product => (
+            <Table.Row
+              key={product._source.id}
+            >
+              <Table.Cell collapsing>
+                {product._source.title}
+              </Table.Cell>
+              <Table.Cell>
+                {
+                  product._source.metafields.map(metafield => (
+                    <div
+                      key={metafield.id}
+                    >
+                      {metafield.key}: {metafield.value}
+                    </div>
+                  ))
+                }
+              </Table.Cell>
+            </Table.Row>
+          ))
+        }
+      </Table.Body>
+    </Table>
   )
 }
 
-
-export default MetafieldsTable
+export default connect(
+  ({ es }) => ({ es }),
+)(MetafieldsTable);
