1d77c28ce8875239f9ed494c46c756f3070a2ab4
Ilia <ilia@acies.ai>
2020-01-21 14:33:07 -0500

    creates a socket connection successfully

--- src/containers/SocketConnection/SocketConnection.js
+++ src/containers/SocketConnection/SocketConnection.js
@@ -1,12 +1,21 @@
 import React, {
   useEffect,
 } from 'react';
+import io from 'socket.io-client';
+
+import { API_URL } from '../../constants';
 
 const SocketConnection = ({
   children,
 }) => {
   useEffect(() => {
     console.log('SocketConnection mounted');
+
+    const socket = io.connect(API_URL);
+
+    socket.on('news', (data) => {
+      console.log('data', data);
+    })
   }, []);
 
   return (
