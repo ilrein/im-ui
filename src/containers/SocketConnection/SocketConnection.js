87602ec90107a2190d343bd27d9a7b380a602fa4
ilia <ilia@acies.ai>
2020-01-28 18:11:39 -0500

    connected SocketManager to trigger a connection based on redux/shop name

--- src/containers/SocketConnection/SocketConnection.js
+++ src/containers/SocketConnection/SocketConnection.js
@@ -2,25 +2,35 @@ import React, {
   useEffect,
 } from 'react';
 import io from 'socket.io-client';
+import { connect } from 'react-redux';
 
 import { API_URL } from '../../constants';
 
 const SocketConnection = ({
   children,
+  session,
 }) => {
   useEffect(() => {
-    console.log('SocketConnection mounted');
+    const { shop } = session;
+    
+    console.log('SocketConnection mounted, trying to join room', shop);
 
     const socket = io.connect(API_URL);
 
     // Topics: [products/create, products/update, products/delete]
 
-    socket.on('news', (data) => {
-      console.log('data', data);
+    socket.on('connect', () => {
+      console.log('connected...', shop);
+
+      socket.emit('room', shop);
     });
 
+    socket.on('update', (data) => {
+      console.log('incoming data', data);
+    })
+
     return () => socket.close();
-  }, []);
+  }, [session]); // eslint-disable-line
 
   return (
     <div>
@@ -29,4 +39,6 @@ const SocketConnection = ({
   )
 }
 
-export default SocketConnection;
+export default connect(
+  ({ session }) => ({ session }),
+)(SocketConnection);
